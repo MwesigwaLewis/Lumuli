88e4f38d7e38b58c0f1fa66b5b3440ab61db1f0f
Mwesigwa Lewis <mwesigwalewis15@gmail.com>
2026-06-08 21:02:56 +0300

    Remove component path from Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,6 @@
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/styles/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
